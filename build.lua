63ff557a7ca971c616ed5121408f044192607c1d
PaulVisscher <paul.pev@hotmail.com>
2016-06-10 22:44:29 +0200

    Removed print

--- build.lua
+++ build.lua
@@ -581,8 +581,6 @@ function zpm.build.loadRoot()
     if zpm.packages.root.dependencies == nil then
         return nil
     end
-
-    print(table.tostring(zpm.packages.root, true))
                 
     for i, dep in ipairs( zpm.packages.root.dependencies ) do
     
