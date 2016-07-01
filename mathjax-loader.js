f64d3defb4e7e92f546c186ba9c40d7fc9dda1d4
PaulVisscher <paul.pev@hotmail.com>
2016-07-01 11:21:22 +0100

    Deployed a8bf2d7 with MkDocs version: 0.15.3

---
+++ mathjax-loader.js
@@ -0,0 +1,26 @@
+/* mathjax-loader.js */
+(function (win, doc) {
+  win.MathJax = {
+    config: ["MMLorHTML.js"],
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX"],
+    tex2jax: {
+      inlineMath: [ ["\\(","\\)"] ],
+      displayMath: [ ["\\[","\\]"] ]
+    },
+    TeX: {
+      TagSide: "right",
+      TagIndent: ".8em",
+      MultLineWidth: "85%",
+      equationNumbers: {
+        autoNumber: "AMS",
+      },
+      unicode: {
+        fonts: "STIXGeneral,'Arial Unicode MS'"
+      }
+    },
+    displayAlign: 'left',
+    showProcessingMessages: false,
+    messageStyle: 'none'
+  };
+})(window, document);
