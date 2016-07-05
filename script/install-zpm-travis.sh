cd0d812587219962eab80fa561f87e4ab757937c
PaulVisscher <paul.pev@hotmail.com>
2016-07-05 09:24:42 +0200

    Elif

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
-fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
 fi
     
