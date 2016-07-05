8bd9a9977159e40bdfeac6103b9a30084ec8fe83
PaulVisscher <paul.pev@hotmail.com>
2016-07-05 09:20:40 +0200

    Syntax fix

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -2,7 +2,7 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
 fi
-f [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
 fi
     
