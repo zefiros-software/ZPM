49790d8e9d4b8d08826da5430c050c8c9732932b
PaulVisscher <paul.pev@hotmail.com>
2016-07-05 08:15:02 +0200

    Osx

--- script/install-zpm-osx.sh
+++ script/install-zpm-osx.sh
@@ -9,7 +9,7 @@ cd ~/zpm-install
 rm premake5.tar.gz || true
 rm premake5 || true
 
-wget -O premake5.tar.gz https://github.com/premake/premake-core/releases/download/v5.0.0-alpha9/premake-5.0.0-alpha9-linux.tar.gz
+wget -O premake5.tar.gz https://github.com/premake/premake-core/releases/download/v5.0.0-alpha9/premake-5.0.0-alpha9-osx.tar.gz
 
 tar xvzf premake5.tar.gz
 chmod a+x premake5
@@ -19,17 +19,11 @@ sudo mkdir /usr/local/zpm/ || true
 sudo mkdir /var/tmp/zpm-cache/ || true
 
 sudo chmod -R 777 /usr/local/zpm/
-sudo setfacl -d -m u::rwX,g::rwX,o::- /usr/local/zpm/
-
 sudo chmod -R 777 /var/tmp/zpm-cache/
-sudo setfacl -d -m u::rwX,g::rwX,o::- /var/tmp/zpm-cache/
 
 ./premake5 --file=zpm/zpm.lua install-zpm
 
 sudo chmod -R 777 /usr/local/zpm/
-sudo setfacl -d -m u::rwX,g::rwX,o::- /usr/local/zpm/
-
 sudo chmod -R 777 /var/tmp/zpm-cache/
-sudo setfacl -d -m u::rwX,g::rwX,o::- /var/tmp/zpm-cache/
 
 rm -rf ~/zpm-install
--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -8,7 +8,7 @@ cd "%TEMP%/zpm-install"
 
 if exist "premake5.zip" del /q "premake5.zip"
 
-powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha8/premake-5.0.0-alpha8-windows.zip -OutFile premake5.zip"
+powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha9/premake-5.0.0-alpha9-windows.zip -OutFile premake5.zip"
 powershell -command Add-Type -AssemblyName System.IO.Compression.FileSystem ^
 
 [System.IO.Compression.ZipFile]::ExtractToDirectory('premake5.zip', '.')"
