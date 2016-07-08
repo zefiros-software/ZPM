79f40a3fdf84e331414330c403f523c6ff280398
PaulVisscher <paul.pev@hotmail.com>
2016-07-08 11:34:08 +0200

    Update git

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -3,6 +3,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
     sudo apt-get update
+    sudo apt-get install git -y
     sudo apt-get install git-lfs -y
     git lfs install
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
