2ed3d07066c70c20438e1503b343c4d4339ff2a5
PaulVisscher <paul.pev@hotmail.com>
2016-07-08 12:32:34 +0200

    Ignore errors

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -3,8 +3,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
     sudo apt-get update
-    sudo rm /usr/local/bin/git-lfs
-    sudo rm /usr/bin/git-lfs
+    sudo rm /usr/local/bin/git-lfs || true
+    sudo rm /usr/bin/git-lfs || true
     sudo apt-get install git-lfs -y
     which git-lfs
     /usr/local/bin/git-lfs install
