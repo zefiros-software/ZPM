5801bf55b897655627c593834499c96f7f5c748d
PaulVisscher <paul.pev@hotmail.com>
2016-07-08 11:07:30 +0200

    Git lfs fix

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
     sudo apt-get install git-lfs=1.2.1
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
-    brew install git-lfs
+    brew update;
+    brew install git-lfs;
 fi
