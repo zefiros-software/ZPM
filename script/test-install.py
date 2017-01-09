bb1b38bd916a65e5f70c40c0b7b2ac1f89080356
PaulVisscher <paul.pev@hotmail.com>
2017-01-09 14:37:29 +0100

    Check both commands

--- script/test-install.py
+++ script/test-install.py
@@ -2,12 +2,13 @@ import subprocess
 import sys
 import os
 
-output = str(subprocess.check_output( ["zpm", "install-package"], universal_newlines=True ))
+for cmd in ["zpm", "premake5"]:
+    output = str(subprocess.check_output( [cmd, "install-package"], universal_newlines=True ))
 
-if ("(c) Zefiros Software" not in output or
-    "Zefiros Package Manager" not in output):
+    if ("(c) Zefiros Software" not in output or
+        "Zefiros Package Manager" not in output):
 
-    print( "ZPM failed to load correctly!\n Output:\n", output )
-    exit(-1)
-else:
-    print( "ZPM successfully installed" )
+        print( "ZPM failed to load correctly!\n Output:\n", output )
+        exit(-1)
+    else:
+        print( "ZPM successfully installed" )
