3272db5e5d107ec12103bd6f626d014dc94d4604
PaulVisscher <paul.pev@hotmail.com>
2017-01-09 14:55:26 +0100

    Accept adding git-core ppa

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -2,7 +2,7 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
     sudo apt-get update
-    sudo add-apt-repository ppa:git-core/ppa
+    sudo add-apt-repository ppa:git-core/ppa -y
     sudo apt-get install git -y
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
     sudo rm /usr/local/bin/git-lfs || true
