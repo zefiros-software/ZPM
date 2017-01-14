b0ba7d57b3c7a1ff3f38c4268fa38eb98ce83fff
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-14 11:36:08 +0100

    Update copyright to zefiros software

--- packages.lua
+++ packages.lua
@@ -1,5 +1,5 @@
 --[[ @cond ___LICENSE___
--- Copyright (c) 2016 Koen Visscher, Paul Visscher and individual contributors.
+-- Copyright (c) 2017 Zefiros Software.
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
