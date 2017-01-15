db8314aad948f313f451220cbfd09db5b1995c7b
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-15 13:07:34 +0100

    Fix version check

--- install.lua
+++ install.lua
@@ -329,8 +329,7 @@ function zpm.install.setup( checkLatest )
         
         local premakeFile = path.join( folder, premakeCmd )
         
-        print(asset.version, zpm.install.minReqVersion)
-        if not os.isfile( premakeFile ) and premake.checkVersion( asset.version, zpm.install.minReqVersion ) then
+        if not os.isfile( premakeFile ) and premake.checkVersion( tostring(asset.version), zpm.install.minReqVersion ) then
     
             printf( "- Installing premake version '%s'", tostring( asset.version ) )
             local file = zpm.util.downloadFromArchive( asset.url, "premake*" )[1]
