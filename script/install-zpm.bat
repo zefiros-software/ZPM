aa03663589642992435edb8005dc4262fe1b2060
PaulVisscher <paul.pev@hotmail.com>
2017-01-17 11:00:26 +0100

    Try different premake version

--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -9,9 +9,9 @@ cd "%TEMP%/zpm-install"
 if exist "premake5.zip" del /q "premake5.zip"
 
 if defined GH_TOKEN (
-    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
+    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha10/premake-5.0.0-alpha10-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
 ) else (
-    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip"
+    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha10/premake-5.0.0-alpha10-windows.zip -OutFile premake5.zip"
 )
 echo Finished downloading premake...
 
@@ -22,7 +22,7 @@ git clone https://github.com/Zefiros-Software/ZPM.git ./zpm
 echo Finished cloning ZPM...
 
 if defined GH_TOKEN (
-    premake5.exe
+    premake5.exe --github-token=%GH_TOKEN% --file=zpm/zpm.lua install-zpm
 ) else (
     premake5.exe --file=zpm/zpm.lua install-zpm
 )
