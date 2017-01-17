86f0519d86ea5ed09e51bd437f7521a23e5adaec
PaulVisscher <paul.pev@hotmail.com>
2017-01-17 12:06:56 +0100

    Remove debug print

--- zpm.lua
+++ zpm.lua
@@ -260,7 +260,6 @@ zpm.modules.setSearchDir()
 
 function zpm.onLoad()
 
-    print("Open loading code")
     if _ACTION == "profile" then
         ProFi = require("mindreframer/ProFi", "@head")
         ProFi:start()
