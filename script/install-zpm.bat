9cde31741848e3a212482a707f642e8b95add611
PaulVisscher <paul.pev@hotmail.com>
2017-01-17 11:06:42 +0100

    Latest premake installer

--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -9,9 +9,9 @@ cd "%TEMP%/zpm-install"
 if exist "premake5.zip" del /q "premake5.zip"
 
 if defined GH_TOKEN (
-    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha10/premake-5.0.0-alpha10-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
+    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
 ) else (
-    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha10/premake-5.0.0-alpha10-windows.zip -OutFile premake5.zip"
+    powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip"
 )
 echo Finished downloading premake...
 
