af6c4dd1fd8304f682aa15247a139b73e66601f7
PaulVisscher <paul.pev@hotmail.com>
2017-01-26 23:36:46 +0100

    When c++14 is defined, remove c++11 flag

--- premake/override.lua
+++ premake/override.lua
@@ -31,6 +31,20 @@ premake.override(path, "normalize", function(base, p)
     return p
 end )
 
+
+premake.override(premake.tools.gcc, "getcxxflags", function(base, cfg)
+   
+    local cxx = base(cfg)
+    if table.contains(cxx, "-std=c++14" ) then
+        local i = table.indexof(cxx, "-std=c++11" )
+        if i then
+            cxx[i] = nil
+        end
+    end
+    return table.filterempty(cxx)
+
+end)
+
 local builtWorkspaces = { }
 
 zpm._workspaceCache = nil
