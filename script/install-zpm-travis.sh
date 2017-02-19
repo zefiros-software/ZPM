15c9b670c185794f407dedb74ae80ef2083abca3
PaulVisscher <paul.pev@hotmail.com>
2017-02-19 11:28:16 +0100

    Update git installer

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -3,15 +3,16 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
 
     apt-get source git
-	cd git_vXXX
+	cd git-1.9.1
 	./configure --prefix=$HOME
 	make
 	make install
 
+	cd ~
 	mkdir -p $HOME/bin
 	wget https://github.com/git-lfs/git-lfs/releases/download/v1.5.6/git-lfs-linux-amd64-1.5.6.tar.gz
 	tar xvfz git-lfs-linux-amd64-1.5.6.tar.gz
-	mv git-lfs-linux-amd64-1.5.6/git-lfs $HOME/bin/git-lfs
+	mv git-lfs-linux-1.5.6/git-lfs $HOME/bin/git-lfs
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
     brew update;
