034459470c6d830939cf90f1562fccc6ed597daa
PaulVisscher <paul.pev@hotmail.com>
2017-02-19 11:26:13 +0100

    Non sudo travis build

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
-    sudo apt-get update
-    sudo add-apt-repository ppa:git-core/ppa -y
-    sudo apt-get install git -y
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    sudo rm -f /usr/local/bin/git-lfs || true
-    sudo rm -f /usr/bin/git-lfs || true
-    sudo apt-get install git-lfs -y
-    /usr/bin/git-lfs install
-    sudo apt-get install g++-multilib -y
+
+    apt-get source git
+	cd git_vXXX
+	./configure --prefix=$HOME
+	make
+	make install
+
+	mkdir -p $HOME/bin
+	wget https://github.com/git-lfs/git-lfs/releases/download/v1.5.6/git-lfs-linux-amd64-1.5.6.tar.gz
+	tar xvfz git-lfs-linux-amd64-1.5.6.tar.gz
+	mv git-lfs-linux-amd64-1.5.6/git-lfs $HOME/bin/git-lfs
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
     brew update;
