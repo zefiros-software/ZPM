3544420a37b1155a6984bc71a692e5e0e6920474
PaulVisscher <paul.pev@hotmail.com>
2017-02-20 20:21:46 +0100

    Install from correct branch

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -3,18 +3,10 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get update
     sudo add-apt-repository ppa:git-core/ppa -y
     sudo apt-get install git -y
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    sudo rm -f /usr/local/bin/git-lfs || true
-    sudo rm -f /usr/bin/git-lfs || true
-    sudo apt-get install git-lfs -y
-    /usr/bin/git-lfs install
-
-    wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm.sh | bash; 
+    wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/features/refactor/script/install-zpm.sh | bash
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/master/script/install-zpm-osx.sh | bash; 
-    brew update;
+    brew update
     brew unlink git
     brew install git
-    brew install git-lfs;
-    git-lfs install
+    wget -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/features/refactor/script/install-zpm-osx.sh | bash
 fi
--- script/install-zpm.sh
+++ script/install-zpm.sh
@@ -2,27 +2,9 @@
 install_dir=~/.zpm_install/
 shared_dir=~/.zpm/zpm/
 cache_dir=~/.zpm/zpm-cache/
-local_install=true
 
-while getopts "g" opt; do
-    case "$opt" in
-    g)        
-        shared_dir=/usr/local/zpm/
-        cache_dir=/var/tmp/zpm-cache/
-        local_install=false
-    ;;
-    esac
-done
-
-SUD=""
-if [ "$local_install" == false ]; then
-  SUD="sudo"
-fi
-
-echo "Shared directory:"
-echo $shared_dir
-echo "Cache directory:"
-echo $cache_dir
+echo "Shared directory: ${shared_dir}"
+echo "Cache directory: ${cache_dir}"
 
 rm -rf $install_dir || true
 rm -rf $shared_dir || true
@@ -32,7 +14,6 @@ mkdir -p $install_dir
 cd $install_dir
 
 rm -f premake5.tar.gz || true
-rm -f premake5 || true
 
 wget -O premake5.tar.gz https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-linux.tar.gz
 
@@ -40,25 +21,13 @@ tar xvzf premake5.tar.gz
 chmod a+x premake5
 git clone https://github.com/Zefiros-Software/ZPM.git ./zpm
 
-${SUD} mkdir -p $shared_dir || true
-${SUD} mkdir -p $cache_dir || true
-
-${SUD} chmod -R 775 $shared_dir
-${SUD} setfacl -d -m u::rwX,g::rwX,o::- $shared_dir
-
-${SUD} chmod -R 775 $cache_dir
-${SUD} setfacl -d -m u::rwX,g::rwX,o::- $cache_dir
+mkdir -p $shared_dir || true
+mkdir -p $cache_dir || true
 
 if [ -z "$GH_TOKEN" ]; then
     ./premake5 --file=zpm/zpm.lua install-zpm
 else
     ./premake5 --github-token=$GH_TOKEN --file=zpm/zpm.lua install-zpm
 fi
-
-${SUD} chmod -R 775 $shared_dir
-${SUD} setfacl -d -m u::rwX,g::rwX,o::- $shared_dir
-
-${SUD} chmod -R 775 $cache_dir
-${SUD} setfacl -d -m u::rwX,g::rwX,o::- $cache_dir
 
 rm -rf $install_dir
