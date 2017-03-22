1736589f4fdcda0fde418f01293dd773b9f58131
p.e.visscher <p.e.visscher@zefiros.eu>
2017-03-22 18:10:15 +0100

    Windows install miniconda

--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -9,6 +9,8 @@ cd "%USERPROFILE%/zpm-install"
 
 if exist "premake5.zip" del /q "premake5.zip"
 
+powershell -command "Invoke-WebRequest -Uri https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile miniconda.exe"
+
 if defined GH_TOKEN (
     powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
 ) else (
@@ -19,6 +21,12 @@ echo Finished downloading premake...
 powershell -command "Expand-Archive premake5.zip -DestinationPath ."
 
 git clone https://github.com/Zefiros-Software/ZPM.git ./zpm --depth 1 -b features/refactor
+
+set ZPM_DIR=./premake5 show install --file=zpm/zpm.lua
+set CONDA_DIR="%ZPM_DIR%/conda/"
+echo %CONDA_DIR%
+start /wait "" miniconda.exe /RegisterPython=0 /AddToPath=0 /S /D=%CONDA_DIR%
+
 
 echo Finished cloning ZPM...
 
