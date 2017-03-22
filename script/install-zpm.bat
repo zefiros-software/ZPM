7ad064142727b6042c3702ef57fc506c8289c1a0
p.e.visscher <p.e.visscher@zefiros.eu>
2017-03-22 22:10:33 +0100

    Fix conda installer

--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -9,7 +9,7 @@ cd "%USERPROFILE%/zpm-install"
 
 if exist "premake5.zip" del /q "premake5.zip"
 
-powershell -command "Invoke-WebRequest -Uri https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile miniconda.exe"
+powershell -command "Invoke-WebRequest -Uri https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile Miniconda4-latest-Windows-x86_64.exe"
 
 if defined GH_TOKEN (
     powershell -command "Invoke-WebRequest -Uri https://github.com/premake/premake-core/releases/download/v5.0.0-alpha11/premake-5.0.0-alpha11-windows.zip -OutFile premake5.zip -Headers @{'Authorization'='token %GH_TOKEN%'}"
@@ -23,10 +23,11 @@ powershell -command "Expand-Archive premake5.zip -DestinationPath ."
 git clone https://github.com/Zefiros-Software/ZPM.git ./zpm --depth 1 -b features/refactor
 
 for /f "tokens=*" %%i in ('".\premake5 show install --file=zpm/zpm.lua"') do set ZPM_DIR=%%i
-set CONDA_DIR=%ZPM_DIR%/conda/
+set CONDA_DIR="ZPM_DIR%/conda/
+for /f "tokens=*" %%i in ('echo %CONDA_DIR:/=\%') do set CONDA_DIR=%%i
 echo %CONDA_DIR%
 echo %ZPM_DIR%
-start /wait "" miniconda.exe /RegisterPython=0 /AddToPath=0 /S /D=%CONDA_DIR%
+start /wait "" Miniconda4-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%CONDA_DIR%
 
 exit
 echo Finished cloning ZPM...
