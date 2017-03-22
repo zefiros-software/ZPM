fcffa8c02fd8b6987019e9ac33fd1ae45b88d605
p.e.visscher <p.e.visscher@zefiros.eu>
2017-03-22 18:28:06 +0100

    Fix batch syntax

--- script/install-zpm.bat
+++ script/install-zpm.bat
@@ -22,7 +22,7 @@ powershell -command "Expand-Archive premake5.zip -DestinationPath ."
 
 git clone https://github.com/Zefiros-Software/ZPM.git ./zpm --depth 1 -b features/refactor
 
-for /f %%i in ('.\premake5 show install --file=zpm/zpm.lua') do set ZPM_DIR=%%i
+for /f "tokens=*" %i in ('.\premake5 show install --file=zpm/zpm.lua') do set ZPM_DIR=%i
 set CONDA_DIR="%ZPM_DIR%/conda/"
 echo %CONDA_DIR%
 start /wait "" miniconda.exe /RegisterPython=0 /AddToPath=0 /S /D=%CONDA_DIR%
