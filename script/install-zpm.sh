f4ba0e88cdc5984a4d45d1f400535d39b33f1424
PaulVisscher <paul.pev@hotmail.com>
2017-03-24 23:55:13 +0100

    Debug py directory

--- script/install-zpm.sh
+++ script/install-zpm.sh
@@ -32,7 +32,7 @@ CONDA_DIR="$ZPM_DIR/conda/"
 chmod a+x miniconda.sh
 bash miniconda.sh -b -p $CONDA_DIR
 
-ls /home/travis/.zpm/conda/bin
+ls /home/travis/.zpm/zpm/py/
 
 if [ -z "$GH_TOKEN" ]; then
     ./premake5 --file=zpm/zpm.lua install zpm
