cef4c9bd2670bc972a24f5ab6d57b905ed6ee07d
PaulVisscher <paul.pev@hotmail.com>
2017-03-24 23:46:17 +0100

    Fix debug command

--- script/install-zpm.sh
+++ script/install-zpm.sh
@@ -32,7 +32,7 @@ CONDA_DIR="$ZPM_DIR/conda/"
 chmod a+x miniconda.sh
 bash miniconda.sh -b -p $CONDA_DIR
 
-ll /home/travis/.zpm/conda/
+ls /home/travis/.zpm/conda/
 
 if [ -z "$GH_TOKEN" ]; then
     ./premake5 --file=zpm/zpm.lua install zpm
