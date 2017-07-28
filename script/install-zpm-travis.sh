80a5a2b7ff46a477e30ad415e39f0114dc25a6a3
PaulVisscher <paul.pev@hotmail.com>
2017-07-28 17:49:35 +0200

    No brew update

--- script/install-zpm-travis.sh
+++ script/install-zpm-travis.sh
@@ -4,7 +4,6 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install git -y
     wget -q -O - https://raw.githubusercontent.com/Zefiros-Software/ZPM/features/refactor/script/install-zpm.sh | bash
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew update
     brew unlink git
     brew install git
     wget -q -O  - https://raw.githubusercontent.com/Zefiros-Software/ZPM/features/refactor/script/install-zpm-osx.sh | bash
